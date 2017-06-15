80feca62c47cc12ff569bdf2cc9299458ed20855
jbmfg <bengoff@gmail.com>
2017-06-15 13:05:35 -0400

    added cutoff value

--- correlate.py
+++ correlate.py
@@ -1,32 +1,33 @@
 import csv, json, re, urllib.parse
 from collections import defaultdict
 
-
 class correlate(object):
-    
+    cutoff_val = 500       #   change this to whatever you want your watchlist runtime to be
     
     def __init__(self, qtime, watchlists):
         self.qtime = qtime
         self.watchlists = watchlists
         
-    def readWL(self):
+    def readWL(self):       
+        '''Read watchlist csv and pull what we want out'''
         wlDict = {}
-        with open(self.watchlists, 'r') as f:
+        with open(self.watchlists, 'r', encoding='utf8') as f:
             readFile = csv.reader(f)
             for x in list(readFile):
                 wlDict["watchlist_" + str(x[0])]=[x[2],urllib.parse.unquote(x[4])]
             f.close()
         return wlDict
 
-    def readqtime(self):
+    def readqtime(self):    
+        '''Read qtime txt and pull what we want out'''
         qtimeDict = defaultdict(list)
-        with open(self.qtime, 'r') as f:
+        with open(self.qtime, 'r', encoding='utf8') as f:
             tempOut = f.read()
             tempOut = urllib.parse.unquote(tempOut)
-            ptrn = re.compile(r"^([0-9]+).+(watchlist_[0-9]+).+(?:=json})$",re.MULTILINE)
+            ptrn = re.compile(r"^([0-9]+).+(watchlist_[0-9]+).+$",re.MULTILINE)
             idAndTimes = re.finditer(ptrn, tempOut)
             for m in idAndTimes:
-                if int(m.group(1))>1000:
+                if int(m.group(1))>= self.cutoff_val:
                     qtimeDict[m.group(2)].append(int(m.group(1)))
                 else:
                     pass
@@ -35,6 +36,7 @@ class correlate(object):
         
         
     def maths(self, qtimeDict):
+        '''build a dictionary with the calculations'''
         results = {}
         for k,v in qtimeDict.items():
             results[k] = {"min":v[-1], "max":v[0], "avg":(sum(v))/len(v), "count":len(v)}
@@ -42,13 +44,14 @@ class correlate(object):
             
             
     def writeCsv(self, results, wlDict):
+        '''Output to CSV file'''
         f = csv.writer(open("SlowestWatchlists.csv", "w", newline=''))
-        f.writerow(["Watchlist ID", "Min", "Max", "Avg", "Count", "Name", "URL Param"])
+        f.writerow(["Watchlist ID", "Name", "Min", "Max", "Avg", "Count", "URL Param"])
         for k in results:
             try:
-                f.writerow([k, results[k]["min"], results[k]["max"], results[k]["avg"], results[k]["count"], wlDict[k][0], wlDict[k][1]])
-            except:
-                pass
+                f.writerow([k, wlDict[k][0], results[k]["min"], results[k]["max"], results[k]["avg"], results[k]["count"], wlDict[k][1]])
+            except: 
+                pass #I dont care about errors ;)
         return True
             
         
@@ -56,7 +59,7 @@ class correlate(object):
 
 
 def main():
-    correlation = correlate('qtime.txt.full', 'watchlists.csv')
+    correlation = correlate('qtime.txt', 'watchlists.csv')
     parsedQTime = correlation.readqtime()
     wlDict = correlation.readWL()
     doMath = correlation.maths(parsedQTime)
@@ -67,3 +70,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
